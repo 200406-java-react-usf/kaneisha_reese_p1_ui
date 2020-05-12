2aaf18a8e101c54cb26412d0706c5ee69861f056
KaneishaR <kaneisha.reese@gmail.com>
2020-05-12 09:25:33 -0500

    setup pipeline

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+//import { AppModule } from "./util/app-module.js";
+
+//export const API_URL = ''
+
+// window.onload = () => {
+//     console.log('page loaded.');
+//     const app = new AppModule();
+//     app.components['login'].render();
+// }
