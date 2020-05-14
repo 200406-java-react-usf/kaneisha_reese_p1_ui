98ba10c2e50cdec7d48d554a5b1aa506d3c482bb
KaneishaR <kaneisha.reese@gmail.com>
2020-05-14 11:26:04 -0500

    kill it with fire 2

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-//import { AppModule } from "./util/app-module.js";
-
-//export const API_URL = ''
-
-// window.onload = () => {
-//     console.log('page loaded.');
-//     const app = new AppModule();
-//     app.components['login'].render();
-// }
