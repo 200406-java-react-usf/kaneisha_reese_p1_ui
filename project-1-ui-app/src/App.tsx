b9ba3e3f48d3d17b0ef583cf6126ee3bf830cc85
KaneishaR <kaneisha.reese@gmail.com>
2020-05-16 07:52:12 -0500

    spun up new app

---
+++ project-1-ui-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState} from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <> 
+        <Router>
+
+        </Router>
+    </>
+  );
+}
+
+export default App;
