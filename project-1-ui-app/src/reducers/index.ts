b812925774ab3d01c513940a0ef2bc31bb706d55
KaneishaR <kaneisha.reese@gmail.com>
2020-05-21 07:13:12 -0500

    aded IUserState

--- project-1-ui-app/src/reducers/index.ts
+++ project-1-ui-app/src/reducers/index.ts
@@ -2,7 +2,9 @@ import { User } from "../dtos/user";
 import { combineReducers } from "redux";
 import { loginReducer } from "./login-reducer";
 import { updateReducer } from "./update-user-reducer"
-
+import { newUserReducer } from "./new-user-reducer"
+import { NewUser } from "../dtos/new-user";
+import { userReducer } from "./user-reducer"
 
 export interface ILoginState {
     authUser: User;
@@ -13,14 +15,32 @@ export interface IUpdateState {
     errorMessage: string;
 }
 
+export interface INewUserState {
+    authUser: User;
+    newUser: NewUser; 
+    errorMessage: string;
+}
+
+export interface IUserState {
+    authUser: User;
+    errorMessage: string;
+}
 
 export interface IState {
     login: ILoginState;
     update: IUpdateState;
+    newUser: INewUserState;
+    user: IUserState
 
 }
 
+
+
 export const state = combineReducers<IState>({
      login: loginReducer,
-     update: updateReducer
+     update: updateReducer,
+     newUser: newUserReducer,
+     user: userReducer
  });
+
+ 
