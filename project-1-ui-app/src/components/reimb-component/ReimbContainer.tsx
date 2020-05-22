5ad626881c489406421abd7cb79b0eec24a7298c
KaneishaR <kaneisha.reese@gmail.com>
2020-05-22 07:42:22 -0500

    added ReimbComponent

---
+++ project-1-ui-app/src/components/reimb-component/ReimbContainer.tsx
@@ -0,0 +1,17 @@
+import { IState } from "../../reducers";
+import { connect } from "react-redux";
+import ReimbComponent from './ReimbComponent';
+
+const mapStateToProps = (state: IState) => {
+    return {
+        authUser: state.login.authUser,
+        errorMessage: ''
+
+    }
+}
+
+const mapDispatchToProps = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReimbComponent);
