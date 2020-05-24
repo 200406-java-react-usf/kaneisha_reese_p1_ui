13eaf878a4e294371c0d327b525e30adf7eb942d
KaneishaR <kaneisha.reese@gmail.com>
2020-05-24 08:31:20 -0500

    changed endpoint to the aws service

--- project-1-ui-app/src/remote/ers-client.ts
+++ project-1-ui-app/src/remote/ers-client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const ersClient = axios.create({
-    baseURL: 'http://localhost:8080',
+    //baseURL: 'http://localhost:8080',
+    baseURL: 'KaneishaReeseP1Api-env.eba-3t4z8zub.us-east-1.elasticbeanstalk.com',
     headers: {
         'Content-Type': 'application/json'
     },
