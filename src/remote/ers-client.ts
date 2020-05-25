de8dc01a6f183a4d80644dd35f2db6a5ae7180d5
KaneishaR <63017525+KaneishaR@users.noreply.github.com>
2020-05-25 17:09:29 -0500

    update baseurl

--- src/remote/ers-client.ts
+++ src/remote/ers-client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const ersClient = axios.create({
     //baseURL: 'http://localhost:8080',
-    baseURL: 'KaneishaReeseP1Api-env.eba-3t4z8zub.us-east-1.elasticbeanstalk.com',
+    baseURL: 'http://kaneishareesep1api-env.eba-3t4z8zub.us-east-1.elasticbeanstalk.com',
     headers: {
         'Content-Type': 'application/json'
     },
