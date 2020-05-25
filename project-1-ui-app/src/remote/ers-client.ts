b41e85692a7fad971c39f7ff3cd5e9fdd03855f2
KaneishaR <kaneisha.reese@gmail.com>
2020-05-25 17:03:44 -0500

    local

--- project-1-ui-app/src/remote/ers-client.ts
+++ project-1-ui-app/src/remote/ers-client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const ersClient = axios.create({
-    //baseURL: 'http://localhost:8080',
-    baseURL: 'KaneishaReeseP1Api-env.eba-3t4z8zub.us-east-1.elasticbeanstalk.com',
+    baseURL: 'http://localhost:8080',
+   // baseURL: 'KaneishaReeseP1Api-env.eba-3t4z8zub.us-east-1.elasticbeanstalk.com',
     headers: {
         'Content-Type': 'application/json'
     },
